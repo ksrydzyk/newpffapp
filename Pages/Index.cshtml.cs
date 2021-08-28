901a670705b860f9bd9ce9ce491285d51c31e154
ksrydzyk <kuc.wojciech@gmail.com>
2021-08-28 21:12:27 +0200

    First commit

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Azure.Core;
+
+
+namespace pffapp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            _logger.LogInformation("INFO: Get request to Index");
+            _logger.LogWarning("WARRNING: Get request to Index");
+            _logger.LogDebug("DEBUG: Get request to Index");
+            _logger.LogError("ERROR: Get request to Index");
+            SecretClientOptions options = new SecretClientOptions()
+                {
+                    Retry =
+                    {
+                        Delay= TimeSpan.FromSeconds(2),
+                        MaxDelay = TimeSpan.FromSeconds(16),
+                        MaxRetries = 5,
+                        Mode = RetryMode.Exponential
+                    }
+                };
+            var client = new SecretClient(new Uri("https://temp-kv.vault.azure.net/"), new DefaultAzureCredential(),options);
+
+            KeyVaultSecret secret = client.GetSecret("ubuntu1pwd");
+
+            string secretValue = secret.Value;
+            ViewData["PWD"] = secretValue;            
+        }
+    }
+}
